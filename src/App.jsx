3e7dc68512967163da4cf5fa851c8507ba06a869
aleksander <aleksander.pawlak@op.pl>
2024-09-06 16:37:25 +0200

    Added editting of the appointment

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { firestore } from './firebase/firebase';
-
 import Paper from '@mui/material/Paper';
-
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import { EditingState, IntegratedEditing, ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
   WeekView,
   MonthView,
   Appointments,
+  AppointmentForm,
   Toolbar,
-  ViewSwitcher
+  ViewSwitcher,
+  AppointmentTooltip,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-
 function App() {
-
-  const [appointments, setAppointments] = useState([]);
+  const [data, setData] = useState([]);
   const [currentDate, setCurrentDate] = useState('2024-09-04');
   const [currentView, setCurrentView] = useState('Day');
 
+
   const fetchAppointments = async () => {
     try {
-      const querySnapshot = await getDocs(collection(firestore, 'appointments'))
-
-      const data = querySnapshot.docs.map(doc => ({
+      const querySnapshot = await getDocs(collection(firestore, 'appointments'));
+      const appointments = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
       }));
-
-      setAppointments(data);
+      console.log(appointments);
+      setData(appointments);
     } catch (error) {
       console.error("Fetching appointments failed...", error);
     }
@@ -45,55 +43,69 @@ function App() {
     setCurrentView(view);
   }
 
-  console.log(appointments);
+  const commitChanges = async ({ added, changed, deleted }) => {
+      let newData = [...data];
+      if (added) {
+        const startingAddedId = newData.length > 0 ? newData[newData.length - 1].id + 1 : 0;
+        newData = [...newData, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        const changedIds = Object.keys(changed);
+      
+        for (let tempId of changedIds) {
+          const changedAppointment = changed[tempId];
+          try {
+            const appointmentToUpdate = newData.find(appointment => appointment.id === tempId);
+            
+            if (!appointmentToUpdate) {
+              console.error(`No matching appointment found for temp ID: ${tempId}`);
+              continue;
+            }
+      
+            const docRef = doc(firestore, 'appointments', appointmentToUpdate.id);
+            const docSnap = await getDoc(docRef);
+            
+            if (!docSnap.exists()) {
+              console.error(`No document to update: ${appointmentToUpdate.id}`);
+              continue;
+            }
 
+            await updateDoc(docRef, changedAppointment);
+            newData = newData.map(appointment =>
+              appointment.id === appointmentToUpdate.id ? { ...appointment, ...changedAppointment } : appointment
+            );
 
-  const addAppointment = async (newAppointment) => {
-    try {
-      await addDoc(collection(firestore, 'appointments'), newAppointment);
-      fetchAppointments();
-    } catch (error) {
-      console.error("Adding appointment failed...", error);
-    }
-  }
-
-  const editAppointment = async (id, updatedAppointment) => {
-    try {
-      const appointmentDoc = doc(firestore, 'appointments', id);
-      await updateDoc(appointmentDoc, updatedAppointment);
-      fetchAppointments();
-    } catch (error) {
-      console.error("Editing appointment failed...", error);
-    }
-  }
-
-  const deleteAppointment = async (id) => {
-    try {
-      const appointmentDoc = doc(firestore, 'appointments', id);
-      await deleteDoc(appointmentDoc);
-      fetchAppointments();
-    } catch (error) {
-      console.error("Deleting appointemt failed...", error);
-    }
-  }
-
+          } catch (error) {
+            console.error("Error updating document: ", error);
+          }
+        }
+      }
+      
+      if (deleted !== undefined) {
+        newData = newData.filter(appointment => appointment.id !== deleted);
+      }
+  };
 
   return (
     <Paper>
-      <Scheduler data={appointments}>
-        
-        <ViewState 
-          currentDate={currentDate} 
+      <Scheduler data={data}>
+        <ViewState
+          currentDate={currentDate}
           onCurrentDateChange={(date) => setCurrentDate(date)}
           currentViewName={currentView}
-          onCurrentViewNameChange={handleViewChange}  
+          onCurrentViewNameChange={handleViewChange}
         />
-
-        <DayView startDayHour={9} endDayHour={14} name="Day"/>
-        <WeekView startDayHour={9} endDayHour={14} name="Week"/>
-        <MonthView name="Month"/>
+        <EditingState onCommitChanges={commitChanges}/>
+        <DayView startDayHour={9} endDayHour={14} name="Day" />
+        <IntegratedEditing />
+        <WeekView startDayHour={9} endDayHour={14} name="Week" />
+        <MonthView name="Month" />
         
         <Appointments />
+        <AppointmentTooltip 
+          showOpenButton
+        />
+        <AppointmentForm/>
         <Toolbar />
         <ViewSwitcher
           currentViewName={currentView}
@@ -101,7 +113,7 @@ function App() {
         />
       </Scheduler>
     </Paper>
-  )
+  );
 }
 
-export default App
+export default App;
